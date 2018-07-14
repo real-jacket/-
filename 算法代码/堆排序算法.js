9e30c4f7a5cd406d5c1cf67c75bb0f3380474096
jacket <1762982273@qq.com>
2018-07-14 19:51:09 +0800

    算法修改

--- 算法代码/堆排序算法.js
+++ 算法代码/堆排序算法.js
@@ -1,26 +1,31 @@
-var A = { 0: 27, 1: 24, 2: 10, 3: 9, 4: 12, 5: 13, 6: 16, 7: 45, 8: 44, 9: 36, 10: 35, 11: 47, 12: 26, 13: 28, 14: 19 };
+var A = [27, 24, 10, 9, 12, 13, 16, 45, 44, 36, 35, 47, 26, 28, 19];
+var B = [];
 //算出二叉堆的层数
-var Swap = function (a, b) {//交换位置函数
-  var t = a;
-  a = b;
-  b = t; 
-};//最大堆调整
-var Max = function (index, left, right) {
-  if (left > right) {
-    if (left > index) {
-      Swap(index, left);
+function Swap(array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+} //最大堆调整
+//Swap(A, 0, 14)
+//console.log(A);
+function Max(array,index, left, right) {
+  if (array[left] > array[right]) {
+    if (array[left] > array[index]) {
+      Swap(array, index, left);
     };
   } else {
-    if (right > index) {
-      Swap(index, right);
+    if (array[right] > array[left]) {
+      Swap(array, index, right);
     };
   };
 };//堆排序
-for (var length = A.length; length > 0; length--) {
-  for (var n=Math.floor(log2(length)); n > 0; n--) {
-    for (var index = 2 ^ n - 2; index > 2 ^ (n - 1) - 2; index--){
-      Max(A[index], A[2 * index + 1], A[2 * index + 2]);
-    };
+for (var Length = A.length; Length > 0; Length--) {
+  var n = Math.floor(Math.log2(Length));
+  for (var index = Math.pow(2,n) - 2; index >= 0; index--){
+    Max(A, index, 2 * index + 1, 2 * index + 2);
   };
-  Swap(A[0], A[length]);
+  Swap(A, 0, Length - 1);
+  B.unshift(A[Length - 1]);
+  A.pop(A[Length - 1]);
 };
+console.log(B);
--- 算法代码/计数排序js实现.js
+++ 算法代码/计数排序js实现.js
@@ -1,30 +1,34 @@
-var A = {0:-1,1:2,2:0,3:4,4:3,5:6,6:5,7:8,8:-2,9:1,10:3,11:0,12:3,13:6,14:5,15:2};
-//1、取出数组里的最大最小值，确定C的长度
+var A = [-1, 2, 0, 4, 3, 6, 5, 8, -2, 1, 3, 0, 3, 6, 5, 2 ];
+//取出数组里的最大最小值，确定C的长度
 var min = A[0],
-    max = A[0],
-    C = {},
-    D = {};
+  max = A[0],
+  C = [],
+  D = [];
 for(var i = 0;i < A.length;i++){
   if(min > A[i]){
-    min = A[i]
+    min = A[i];
   };
   if(max < A[i]){
-    max = A[i]
+    max = A[i];
   };
 };
 C.length = max - min + 1;
+var index = min;
+for (var i = 0; i < C.length; i++) {
+  C[i] = 0;
+}
 //对A中的数字出现次数计数,得出计数后的C数组
-for(var i = 0;i < C.length;i++){
-  for(var j = 0;j < A.length;j++){
-    if(A[j]=min){
+for (var i = 0; i < C.length; i++){
+  for (var j = 0; j < A.length; j++){
+    if(A[j]==index){
       C[i]++;
     };
   };
-  min++;
+  index++;
 }
 //得出A计数后的形成的D数组
-for(var i = 0;i < C.length;i++){
-  for(var j = 0;j < C[i];j++){
+for (var i = 0; i < C.length; i++){
+  for (var j = 0; j < C[i]; j++){
     D.push(min + i);
   };
 };
